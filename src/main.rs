92632cc7ea4dd27906a65c3ae8e251467b87745d
f.grezes <f.grezes@exatech-group.com>
2022-07-18 13:55:38 +0200

    add basic goban example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use goban::rules::{game::Game, Move, CHINESE};
+use rand::prelude::IteratorRandom;
+
 fn main() {
-    println!("Hello, world!");
+    let mut gb = Game::builder()
+        .size((19, 19))
+        .rule(CHINESE)
+        .build()
+        .unwrap();
+
+    let mut i = 35;
+    while !gb.is_over() && i > 0 {
+        gb.play(
+            gb.legals()
+                .choose(&mut rand::thread_rng())
+                .map(|p| Move::Play(p.0, p.1))
+                .unwrap(),
+        );
+
+        gb.display_goban();
+
+        println!("{:?}", gb.outcome());
+
+        i -= 1;
+    }
 }
